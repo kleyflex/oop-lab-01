1b9bb37ee98712d3c88ea8a9bd59848307b7b48d
kleyflex <kleyfie92@gmail.com>
2026-09-10 00:19:12 +0300

    feat: add Hero class to change strategy

---
+++ src/game/Hero.java
@@ -0,0 +1,44 @@
+package game;
+import game.movement.MovementStrategy;
+
+public class Hero {
+    private final String name;
+
+    private Point position;
+
+    private MovementStrategy movement;
+
+    public Hero(String name, Point start, MovementStrategy movement) {
+        this.name = name;
+        this.position = start;
+
+        setMovement(movement);
+    }
+
+    public void setMovement(MovementStrategy movement) {
+        if(movement == null) {
+            throw new IllegalArgumentException("Способ перемещения не задан");
+        }
+
+        this.movement = movement;
+    }
+
+    public MovementStrategy getMovement() {
+        return movement;
+    }
+
+    public void move(Point destination) {
+        movement.move(name, position, destination);
+        this.position = destination;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+
+}
