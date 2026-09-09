8be92a0f1ba758e66bc2c883e70461488630b0c6
kleyflex <kleyfie92@gmail.com>
2026-09-09 22:39:50 +0300

    feat: add class Point

---
+++ src/game/Point.java
@@ -0,0 +1,14 @@
+package game;
+
+// Точка на карте
+public class Point {
+    private final String name;
+
+    public Point(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
